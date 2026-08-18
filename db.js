7e2e8adc255cb281335d68c75d92dd0a1610c92e
William Dacoula <ton.email@gmail.com>
2026-08-18 01:03:20 +0000

    Fix SSL connection terminated

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: process.env.DATABASE_URL && process.env.DATABASE_URL.includes('localhost') 
+    ? false 
+    : { rejectUnauthorized: false }
 });
 
 pool.connect((err, client, release) => {
